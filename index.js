aa9103913b2690b9d990522fb688d7d16090f47b
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2022-11-26 11:29:53 +0530

    npm init

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const http = require('http');
+const colors=require('colors')
 
+const http = require('http');
 
 http.createServer((req,resp) =>
 {
 resp.write("<h1>Code Step by step</h1>");
 resp.end();
 }).listen(4500);
+
+console.log("Hello".red + " Vivek Srivas   tava".green) 
