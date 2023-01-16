86035894d639fd1e0ba2ce096c491d1afae5cc0d
Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>
2023-01-17 01:04:31 +0530

    remove extension from url

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const path = require('path');
 const app = express();
 const publicPath=path.join(__dirname,'public')
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+app.get('/' , (req,resp)=>{
+    resp.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/about' , (req,resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('/help' , (req,resp)=>{
+    resp.sendFile(`${publicPath}/help.html`)
+})
 
 app.listen(5000);
